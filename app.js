3f3418a620a6af19a2a3065ea070659ea813a9eb
francoiturco <francoiturco@gmail.com>
2021-04-17 10:07:40 -0300

    con el css solicitado

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/ofertas', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ofertas.html'));
+});
+
+app.get('/tiendasOficiales', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/tiendasOficiales.html'));
+});
+
+app.get('/vender', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/vender.html'));
+});
+
+app.get('/ayuda', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ayuda.html'));
+});
+
+app.get('/creaTuCuenta', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/creaTuCuenta.html'));
+});
+
+app.get('/ingresa', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/ingresa.html'));
+});
+
+app.get('/misCompras', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/misCompras.html'));
+});
+
+app.listen('3001', () => console.log('Servidor Andando'));
